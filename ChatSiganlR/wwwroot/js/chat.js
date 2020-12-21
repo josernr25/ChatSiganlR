f90988f50ef84027ecb19624e4f6c6c1493dcca7
Jose Molina <josernr25@gmail.com>
2020-12-21 15:52:59 -0300

    Agregar archivos de proyecto.

---
+++ ChatSiganlR/wwwroot/js/chat.js
@@ -0,0 +1,26 @@
+﻿// Se crea la conexion
+const connection = new signalR.HubConnectionBuilder().withUrl("/chat").build();
+
+// Metodo que recive los msj
+connection.on("ReceiveMessage", (name, message) => {
+    const fecha = new Date().toLocaleTimeString();
+    const messageView = fecha + " - <strong>" + name + " :</strong> " + message;
+    const li = document.createElement("li");
+    li.innerHTML = messageView;
+    document.getElementById("chat").appendChild(li);
+});
+
+// Se activa la conexion
+connection.start().catch(err => console.error(err.toString()));
+
+document.getElementById("sendButton").addEventListener("click", event => {
+    const user = document.getElementById("userInput").value;
+    const message = document.getElementById("messageInput").value;
+
+    console.log("Entra al evento: " + user + " " + message);
+
+    // Se envia el mensaje al Hub
+    connection.invoke("SendMessage", user, message).catch(err => console.error(err.toString()));
+
+    event.preventDefault();
+});
